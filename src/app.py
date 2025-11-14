ff76cd4eaec3903e1a5effb95188633fa176b982
feycry <feycry@gmail.com>
2025-11-14 16:16:48 +0200

    Add all fields to reference creation

--- src/app.py
+++ src/app.py
@@ -16,11 +16,44 @@ def new():
 
 @app.route("/create_reference", methods=["POST"])
 def reference_creation():
+    # Required fields
+    author = request.form.get("author")
     title = request.form.get("title")
+    booktitle = request.form.get("booktitle")
+    year = request.form.get("year")
+    
+    # Optional fields
+    url = request.form.get("url")
+    doi = request.form.get("doi")
+    editor = request.form.get("editor")
+    volume = request.form.get("volume")
+    number = request.form.get("number")
+    series = request.form.get("series")
+    pages = request.form.get("pages")
+    address = request.form.get("address")
+    month = request.form.get("month")
+    organization = request.form.get("organization")
+    publisher = request.form.get("publisher")
 
     try:
         validate_reference(title)
-        create_reference(title)
+        create_reference(
+            author=author,
+            title=title,
+            booktitle=booktitle,
+            year=year,
+            url=url,
+            doi=doi,
+            editor=editor,
+            volume=volume,
+            number=number,
+            series=series,
+            pages=pages,
+            address=address,
+            month=month,
+            organization=organization,
+            publisher=publisher
+        )
         return redirect("/")
     except Exception as error:
         flash(str(error))
--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -8,7 +8,28 @@ def get_references():
     references = result.fetchall()
     return [Reference(reference[0], title=reference[1]) for reference in references] 
 
-def create_reference(title):
-    sql = text('INSERT INTO refs (title) VALUES (:title)')
-    db.session.execute(sql, { "title": title })
+def create_reference(author, title, booktitle, year, url=None, doi=None, editor=None, 
+                     volume=None, number=None, series=None, pages=None, address=None, 
+                     month=None, organization=None, publisher=None):
+    sql = text('''INSERT INTO refs (author, title, booktitle, year, url, doi, editor, 
+                  volume, number, series, pages, address, month, organization, publisher) 
+                  VALUES (:author, :title, :booktitle, :year, :url, :doi, :editor, 
+                  :volume, :number, :series, :pages, :address, :month, :organization, :publisher)''')
+    db.session.execute(sql, {
+        "author": author,
+        "title": title,
+        "booktitle": booktitle,
+        "year": year,
+        "url": url,
+        "doi": doi,
+        "editor": editor,
+        "volume": volume,
+        "number": number,
+        "series": series,
+        "pages": pages,
+        "address": address,
+        "month": month,
+        "organization": organization,
+        "publisher": publisher
+    })
     db.session.commit()
