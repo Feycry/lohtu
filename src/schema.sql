0d190d4e3e62f69d3ad502585802c21a66b9928d
Vili Mähönen <vilimahonen@icloud.com>
2025-11-14 13:49:45 +0200

    schema päivitetty

--- src/schema.sql
+++ src/schema.sql
@@ -3,3 +3,26 @@ CREATE TABLE todos (
   content TEXT NOT NULL,
   done BOOLEAN DEFAULT FALSE
 )
+
+CREATE TABLE sources (
+    id SERIAL PRIMARY KEY,
+    author TEXT,
+    title TEXT,
+    booktitle TEXT,
+    year INT,
+    editor TEXT,
+    volume INT,
+    number INT,
+    series TEXT,
+    pages TEXT,
+    address TEXT,
+    month TEXT,
+    organization TEXT,
+    publisher TEXT,
+    url TEXT,
+    doi TEXT
+);
+
+CREATE TABLE sourceType (
+    id SERIAL PRIMARY KEY
+);
