9394effa99acb6c7096a77cceddde99549d0f024
feycry <feycry@gmail.com>
2025-11-21 13:44:06 +0200

    Refactor schema to support required fields for reference types

--- src/schema.sql
+++ src/schema.sql
@@ -1,5 +1,12 @@
+CREATE TABLE refType (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    required_fields TEXT[] NOT NULL
+);
+
 CREATE TABLE refs (
     id SERIAL PRIMARY KEY,
+    ref_type_id INTEGER REFERENCES refType(id),
     author TEXT,
     title TEXT,
     booktitle TEXT,
@@ -14,9 +21,28 @@ CREATE TABLE refs (
     address TEXT,
     month TEXT,
     organization TEXT,
-    publisher TEXT
+    publisher TEXT,
+    edition TEXT,
+    howpublished TEXT,
+    institution TEXT,
+    journal TEXT,
+    note TEXT,
+    school TEXT,
+    type TEXT
 );
 
-CREATE TABLE refType (
-    id SERIAL PRIMARY KEY
-);
+INSERT INTO refType (name, required_fields) VALUES 
+    ('article', ARRAY['author', 'title', 'journal', 'year']),
+    ('book', ARRAY['author', 'title', 'publisher', 'year']),
+    ('booklet', ARRAY['title']),
+    ('conference', ARRAY['author', 'title', 'booktitle', 'year']),
+    ('inbook', ARRAY['author', 'title', 'booktitle', 'publisher', 'year']),
+    ('incollection', ARRAY['author', 'title', 'booktitle', 'publisher', 'year']),
+    ('inproceedings', ARRAY['author', 'title', 'booktitle', 'year']),
+    ('manual', ARRAY['title']),
+    ('mastersthesis', ARRAY['author', 'title', 'school', 'year']),
+    ('misc', ARRAY[]::TEXT[]),
+    ('phdthesis', ARRAY['author', 'title', 'school', 'year']),
+    ('proceedings', ARRAY['title', 'year']),
+    ('techreport', ARRAY['author', 'title', 'institution', 'year']),
+    ('unpublished', ARRAY['author', 'title']);
